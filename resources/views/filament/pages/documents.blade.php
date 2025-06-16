653c69503803ea4a94a582be321c30302d8b13f8
jonathan.guevara@silent4business.com <jonathan.guevara@silent4business.com>
2025-06-16 15:39:59 -0600

    Fix documents view

--- resources/views/filament/pages/documents.blade.php
+++ resources/views/filament/pages/documents.blade.php
@@ -206,9 +206,8 @@
         <div x-show="tab === 'BD'" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             
         @foreach ($filteredDocuments as $doc) 
-            @if (is_null($doc['file_type']))
-            <x-filament::card class="hover:shadow-lg transition-shadow">
-                
+            @if ($doc['file_type'] == 'BD')            
+            <x-filament::card class="hover:shadow-lg transition-shadow">    
                 <div class="flex justify-between items-start">
                     <div>
                         <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
